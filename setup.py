d518df95f5508baa474aa4e53665c5b7acf132d0
Christian Haudum <christian.haudum@gmail.com>
2018-10-23 21:55:58 +0200

    add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -32,5 +32,15 @@ setup(
         ]
     },
     python_requires='>=3.6',
-    install_requires=requirements
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
